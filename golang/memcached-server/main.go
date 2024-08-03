6f3cfb5ffc335f0f9c76a470bff5f89151d949fc
henryjw <3443648+henryjw@users.noreply.github.com>
2024-08-03 17:40:05 -0400

    accept commands from clients

--- golang/memcached-server/main.go
+++ golang/memcached-server/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"errors"
+	"bufio"
 	"fmt"
 	"github.com/urfave/cli/v2"
 	"log"
+	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -29,5 +31,77 @@ func main() {
 }
 
 func run(portNumber int) error {
-	return errors.New(fmt.Sprintf("attempted to run app on port %d. However, this functionality is not yet implemented", portNumber))
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", portNumber))
+
+	if err != nil {
+		return fmt.Errorf("error starting server: %v", err)
+	}
+
+	log.Printf("Server listening on port %d\n", portNumber)
+
+	defer func() {
+		closeErr := listener.Close()
+		if closeErr != nil {
+			log.Println("Error closing listener", err)
+			return
+		}
+
+		log.Println("Listener closed")
+	}()
+
+	handleConnections(listener)
+
+	return nil
+}
+
+// handleConnections this is a blocking call
+func handleConnections(listener net.Listener) {
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("Error accepting connection", err)
+			continue
+		}
+
+		fmt.Println("Accepted new connection")
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer func() {
+		closeErr := conn.Close()
+		if closeErr != nil {
+			log.Println("Error closing connection: ", closeErr)
+			return
+		}
+
+		log.Printf("Successfully closed connection")
+	}()
+
+	reader := bufio.NewReader(conn)
+	writer := bufio.NewWriter(conn)
+	//buffer := make([]byte, 0)
+
+	for {
+		message, readErr := reader.ReadString('\n')
+		message = strings.TrimSpace(message)
+
+		if readErr != nil {
+			fmt.Println("Error reading from connection: ", readErr)
+			continue
+		}
+
+		fmt.Printf("Message received: '%s'\n", message)
+
+		// TODO: implement actual functionality. This just echoes the message back to the client
+		_, writeErr := writer.WriteString(fmt.Sprintf("Echo: %v\n", message))
+		if writeErr != nil {
+			fmt.Println("Error sending message: ", writeErr)
+			continue
+		}
+
+		writer.Flush()
+	}
 }
